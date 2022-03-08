7596c1e0475b7704cde73d6749e56dde24bff2bb
kimminkyu <elliotelon@gmail.com>
2022-03-08 17:16:09 +0900

    Styled-components 설정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,17 @@
 import { NextPage } from "next";
+import styled from "styled-components";
+
+const Container = styled.div`
+  padding: 20px;
+`;
 
 const index: NextPage = () => {
-  return <div>hello Typescript</div>;
+  return (
+    <Container>
+      <h1>hello Styled-components</h1>
+      <h2>hello Styled-components</h2>
+    </Container>
+  );
 };
 
 export default index;
