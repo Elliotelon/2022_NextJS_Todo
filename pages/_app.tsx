d9fc38e9b058c97b77d407103eaa102037be014d
kimminkyu <elliotelon@gmail.com>
2022-03-12 00:13:39 +0900

    푸터 만들기

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import App, { AppContext, AppProps, AppInitialProps } from "next/app";
 import GlobalStyle from "../styles/GlobalStyle";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const app = ({ Component, pageProps }: AppProps) => {
   return (
@@ -8,6 +9,7 @@ const app = ({ Component, pageProps }: AppProps) => {
       <GlobalStyle />
       <Header />
       <Component {...pageProps} />
+      <Footer />
     </>
   );
 };
